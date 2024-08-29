dae7dbed16be54f1268e8d5254658cdacbafa3d9
Guilherme Camurça Chagas <153216205+guiOchagas@users.noreply.github.com>
2024-08-29 03:14:27 -0300

    Add files via upload

---
+++ PYTHON/ex001.py
@@ -0,0 +1,2 @@
+name = input('Por favor, digite o seu nome: ')
+print('Nice to meet you, {}! Você vai ver como eu serei bom nisso'.format(name))
---
+++ PYTHON/ex002.py
@@ -0,0 +1,4 @@
+n1 = (input('Escreva algo: '))
+print('Isso é numerico? ', n1.isnumeric())
+print('Isso é caracteres? ', n1.isalpha())
+print('Isso é capitalizado? ', n1.istitle())
