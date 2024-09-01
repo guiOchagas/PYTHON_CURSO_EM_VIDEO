2bde76b27ad05a66d602322b7083eef73d2413c7
Guilherme Camurça Chagas <153216205+guiOchagas@users.noreply.github.com>
2024-09-01 01:35:07 -0300

    Create ex037.py

---
+++ PYTHON/ex037.py
@@ -0,0 +1,9 @@
+n1 = int(input('Digite o número 1: '))
+n2 = int(input('Digite o número 2: '))
+if n1 > n2:
+    print('O número {} é maior que o {}'.format(n1, n2))
+elif n1 < n2:
+    print('O número {} é maior que o {}'.format(n2, n1))
+elif n1 == n2:
+    print('Ambos os números são iguais.')
+    
