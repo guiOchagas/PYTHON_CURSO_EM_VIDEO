a8de60368dcd0c6b39c83a0f5a406d39c4ad6d4f
Guilherme Camurça Chagas <153216205+guiOchagas@users.noreply.github.com>
2024-09-06 02:50:36 -0300

    Create ex069.py

---
+++ PYTHON/ex069.py
@@ -0,0 +1,32 @@
+homem = mulher = maior = menor = cont = 0
+
+while True:
+    idade = int(input('Digite a idade: '))
+    sexo = str(input('Digite o sexo: ')).upper()
+    
+    if idade >= 18: #MAIOR DE IDADE
+        maior += 1
+    
+    if sexo == 'M': #HOMENS
+        homem += 1
+        
+    if sexo == 'F': #MULHERES
+        mulher += 1
+        
+    if sexo == 'F' and idade < 18:
+        menor += 1
+
+    cont += 1
+
+    print()
+    go = str(input('Deseja continuar? [S/N]: ')).upper()
+    print()
+    if go == 'N':
+        break
+
+print()
+print(f"""PESSOAS CADASTRADAS: {cont}
+MAIOR DE IDADE: {maior}
+HOMENS: {homem}
+MULHERES: {mulher}
+MULHERES -18: {menor}""")
