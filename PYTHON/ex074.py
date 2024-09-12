8cb90b98b8ff6359fba3a4f823b3baaeedad18af
Guilherme Camurça Chagas <153216205+guiOchagas@users.noreply.github.com>
2024-09-11 22:52:14 -0300

    Update ex074.py

--- PYTHON/ex074.py
+++ PYTHON/ex074.py
@@ -1,6 +1,12 @@
 from random import randint
-num = (randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10), randint(0, 10))
-for n in num:
-    print(f'{n} ', end='')
-print(f'\nO maior valor é o {max(num)}')
-print(f'O menor valor é o {min(num)}')
+tupla = (randint(0, 9), randint(0, 9), randint(0, 10), randint(0, 9), randint(0, 9), )
+ext = ('um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove','dez','onze','doze','treze','quatorze','quinze', 'dezesseis', 'dezessete', 'dezoito')
+for c in tupla:
+    print(f'{c}', end=' ')
+print()
+print(f'Maior: [{max(tupla)}]')
+print(f'Menor: [{min(tupla)}]')
+print(f'E a soma entre eles é [{max(tupla)+min(tupla)}]')
+soma = max(tupla) + min(tupla)
+print()
+print(f'Esse número por escrito é: [{ext[soma - 1]}]')
