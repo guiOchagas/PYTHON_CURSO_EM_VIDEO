4b5f65833a3ece93a642d70eaf360f4956f30b2b
Guilherme Camurça Chagas <153216205+guiOchagas@users.noreply.github.com>
2024-09-11 22:21:03 -0300

    Update ex072.py

--- PYTHON/ex072.py
+++ PYTHON/ex072.py
@@ -1,11 +1,15 @@
+from time import sleep
 num = ('um', 'dois', 'três', 'quatro', 'cinco')
 while True:
     dig = int(input('Digite um número de [1] à [5], ou [0] para sair. '))
     if dig >= 1 and dig <= 5:
         print(f'O número [{dig}] por extenso é {num[dig-1].upper()}')
         print()
-    elif dig > 5:
+    elif dig == 0:
+        print('Encerrando...')
+        sleep(2)
+        print('Até a próxima!')
+        break
+    else:
         print('Opção inválida.')
         print()
-    else:
-        break
