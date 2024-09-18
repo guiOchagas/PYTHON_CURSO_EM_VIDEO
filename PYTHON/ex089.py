bee11f2ae796d91c9399892df5e1e1cb976bbf79
Guilherme Camurça Chagas <153216205+guiOchagas@users.noreply.github.com>
2024-09-18 02:09:01 -0300

    Update ex089.py

--- PYTHON/ex089.py
+++ PYTHON/ex089.py
@@ -1,11 +1,14 @@
 lista = []
+
 while True:
     nome = str(input('Nome: '))
     nota1 = float(input('Nota 1: '))
     nota2 = float(input('Nota 2: '))
     media = (nota1 + nota2) / 2
     print()
+    
     lista.append([nome, [nota1, nota2], media])
+    
     resp = str(input('Continuar? [S/N] '))
     print()
     if resp in 'Nn':
@@ -20,10 +23,13 @@ for i, p in enumerate(lista):
 while True:
     print()
     print('=' * 30)
+    
     opcao = int(input('Digite o nº do aluno para ver as notas (999 finaliza): '))
     print()
+    
+    if opcao <= len(lista) - 1:
+        print(f'As notas de {lista[opcao][0]} foram {lista[opcao][1]}\nPor isso ele(a) ficou com a média {lista[opcao][2]}')
+        
     if opcao == 999:
         print('FUI...')
         break
-    if opcao <= len(lista) - 1:
-        print(f'As notas de {lista[opcao][0]} foram {lista[opcao][1]}\nPor isso ele(a) ficou com a média {lista[opcao][2]}')
