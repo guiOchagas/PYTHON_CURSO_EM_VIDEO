79d9d3fe61e6213b8dd5bc6666a206816048b3c5
Guilherme Camurça Chagas <153216205+guiOchagas@users.noreply.github.com>
2024-09-26 02:50:28 -0300

    Create ex097.py

---
+++ PYTHON/ex097.py
@@ -0,0 +1,10 @@
+def escreva(txt):
+    tam = len(txt) + 4
+    print('=' * tam)
+    print(f'  {txt}')
+    print('=' * tam)
+
+
+escreva('GUILHERME')
+escreva('oi')
+escreva('Learning to develop in Python')
