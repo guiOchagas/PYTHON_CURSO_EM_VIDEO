1a494469322ebf96f912892491e758578b15d826
Guilherme Camurça Chagas <153216205+guiOchagas@users.noreply.github.com>
2024-09-26 01:28:57 -0300

    Update ex091.py

--- PYTHON/ex091.py
+++ PYTHON/ex091.py
@@ -1,7 +1,8 @@
 from random import randint
 from time import sleep
+from operator import itemgetter
 
-jogo = {'jogador_1': randint(1,6), 'jogador_2': randint(1,6), 'jogador_3': randint(1,6), 'jogador_4': randint(1,6)}
+jogo = {'jogador_1': randint(1,9), 'jogador_2': randint(1,9), 'jogador_3': randint(1,9), 'jogador_4': randint(1,9)}
 print('='*10)
 print(f'   JOGO')
 print('='*10)
@@ -13,4 +14,10 @@ print()
 print('='*10)
 print(f' RESULTADO')
 print('='*10)
+
+ranking = {}
+ranking = sorted(jogo.items(), key=itemgetter(1), reverse=True)
+print(ranking)
+
+
 print()
